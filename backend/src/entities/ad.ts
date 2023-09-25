f0e837ea1896c1fe8a0a11a9785463c42a80b1b3
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-09-25 12:48:27 +0200

    test entitie function .. non foncionnel TYPEORM oblige les class

--- backend/src/entities/ad.ts
+++ backend/src/entities/ad.ts
@@ -1,54 +1,55 @@
 import {
-    Entity,
-    BaseEntity,
-    PrimaryGeneratedColumn,
-    CreateDateColumn,
-    Column,
-    ManyToOne,
-    JoinTable,
-    ManyToMany,
-  } from "typeorm";
-  import { Length, Min } from "class-validator";
-  import { Category } from "./category";
-  import { Tag } from "./tag";
-  
-  @Entity()
-  export class AdEntities extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column({ length: 50 })
-    @Length(5, 50, { message: "Le titre doit contenir entre 5 et 50 caractères" })
-    title: string;
-  
-    @Column({ nullable: true, type: "text" })
-    description: string;
-  
-    @Column()
-    owner: string;
-  
-    @Column({ type: "float" })
-    @Min(0, { message: "le prix doit etre positif" })
-    price: number;
-  
-    @Column()
-    location: string;
-  
-    @Column()
-    picture: string;
-  
-    @CreateDateColumn()
-    createdAt: string;
-  
-    @ManyToOne(() => Category, (c) => c.ads, {
-      cascade: true,
-      onDelete: "CASCADE",
-    })
-    category: Category;
-  
-    @JoinTable()
-    @ManyToMany(() => Tag, (t) => t.ads, {
-      cascade: true,
-    })
-    tags: Tag[];
-  }
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Column,
+  ManyToOne,
+  JoinTable,
+  ManyToMany,
+} from "typeorm";
+import { Length, Min } from "class-validator";
+import { Category } from "./category";
+import { Tag } from "./tag";
+
+@Entity()
+export function AdEntities() {
+  const id = PrimaryGeneratedColumn();
+
+  const title = Column({ length: 50 });
+  Length(5, 50, { message: "Le titre doit contenir entre 5 et 50 caractères" })(this, "title");
+
+  const description = Column({ nullable: true, type: "text" });
+
+  const owner = Column();
+
+  const price = Column({ type: "float" });
+  Min(0, { message: "le prix doit être positif" })(this, "price");
+
+  const location = Column();
+
+  const picture = Column();
+
+  const createdAt = CreateDateColumn();
+
+  const category = ManyToOne(() => Category, (c) => c.ads, {
+    cascade: true,
+    onDelete: "CASCADE",
+  });
+
+  const tags = ManyToMany(() => Tag, (t) => t.ads, {
+    cascade: true,
+  });
+
+  return {
+    id,
+    title,
+    description,
+    owner,
+    price,
+    location,
+    picture,
+    createdAt,
+    category,
+    tags,
+  };
+}
