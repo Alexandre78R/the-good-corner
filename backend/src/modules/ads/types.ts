691364079e3e6f7a9d063b3f53626f6f664ffabe
Alexandre Renard <16491342+Alexandre78R@users.noreply.github.com>
2023-09-28 19:12:13 +0200

    amélioration structures Ad

--- backend/src/modules/ads/types.ts
+++ backend/src/modules/ads/types.ts
@@ -1,10 +1,32 @@
 import { Request } from "express";
+import { Category } from "../categories/entity";
+import { Tag } from "../tags/entity";
 
 interface CustomRequestUpdate extends Request {
     sqlUpdate?: string;
     sqlParams?: any[];
 }
 
+interface Ad {
+    id: number;
+    title: string;
+    description: string;
+    owner: string;
+    price: number;
+    location: string;
+    picture: string;
+    createdAt: Date;
+    updatedAt: Date;
+    category: Category;
+    tags: Tag[];
+}
+
+  interface IAdForm extends Omit<Ad, "createdAt" | "updatedAt" | "tags"> {
+    id?: number; // Maintenant, id est obligatoire et de type number
+    category: Omit<Category, "name">;
+}
+
 export { 
-    CustomRequestUpdate 
+    CustomRequestUpdate,
+    IAdForm
 }
